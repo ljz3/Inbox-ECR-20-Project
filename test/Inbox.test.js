340692b139107fe7ee443d3b8ccf0cda81fa81dd
ljz3 <kevin.ottawa@hotmail.com>
2020-12-28 20:37:25 -0500

    Using mocha for testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,21 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+
+    // get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    // use one of those accounts to deploy the contract
+
+
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {});
+});
